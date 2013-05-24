3dc6d7a7df43a42099b158fe72a1623048a7e168
Naomi Guyer <naomi@silverstripe.com>
2013-05-24 15:48:15 +1200

    BUG: Fix menu-icon to work with 3.1

--- code/TaxonomyAdmin.php
+++ code/TaxonomyAdmin.php
@@ -12,6 +12,8 @@ class TaxonomyAdmin extends ModelAdmin {
 	private static $managed_models = array('TaxonomyTerm');
 
 	private static $menu_title = 'Taxonomies';
+	
+	private static $menu_icon = "taxonomy/images/tag.png";
 
 	public function getList() {
 		$list = parent::getList();
