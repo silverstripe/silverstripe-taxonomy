4fd2d05218a5b9fbaa73d5832b4be4971f812d6d
Guy Sartorelli <36352093+GuySartorelli@users.noreply.github.com>
2025-08-21 14:51:39 +1200

    ENH Reduce count queries for scaffolding relations (#135)

--- src/TaxonomyTerm.php
+++ src/TaxonomyTerm.php
@@ -149,8 +149,11 @@ class TaxonomyTerm extends DataObject implements PermissionProvider
         $field = SearchableMultiDropdownField::create($relationName, $fieldTitle, $list, labelField: 'Name');
         // Use the same lazyload threshold has_one relations use
         $threshold = DBForeignKey::config()->get('dropdown_field_threshold');
-        $overThreshold = $list->count() > $threshold;
-        $field->setIsLazyLoaded($overThreshold)->setLazyLoadLimit($threshold);
+        $overThreshold = $threshold === 0 || $list->setUseCache(true)->count() > $threshold;
+        $field->setIsLazyLoaded($overThreshold);
+        if ($threshold > 0) {
+            $field->setLazyLoadLimit($threshold);
+        }
         return $field;
     }
 
